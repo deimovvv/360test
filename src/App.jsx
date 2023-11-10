e64a193b0da8f8060f202471633c69e3888655fe
Gonzalo <97199356+deimovvv@users.noreply.github.com>
2023-11-10 16:36:44 -0300

    aaaaasjsj

--- src/App.jsx
+++ src/App.jsx
@@ -1,111 +1,63 @@
-import React, { useEffect, useRef } from 'react';
-import ReactPlayer from "react-player";
-import styled from "styled-components";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container} from "react-bootstrap"
+import React, { useRef, useEffect } from 'react';
+import ReactPlayer from 'react-player';
+import styled from 'styled-components';
 
-
-
-
-const StyledVideoPlayer = styled(ReactPlayer)`
-  width: 100%;
-  height: 100%;
-  border: 1px solid #ccc;
-  /* Agrega cualquier otro estilo que desees */
-  position: absolute;
+const PlayerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
 `;
 
 
 
-const PlayerWrapper = styled.div`
-position: relative;
-
-`
-
 const ContainerTest = styled.div`
-width: 100px;
-height: 100px;
-background-color: #dcc36f;
-z-index: 1000;
-position: absolute;
-right: 800px;
-top: 200px;
-border-radius: 20px;
-cursor: pointer;
-`
+  width: 100px;
+  height: 100px;
+  background-color: #dcc36f;
+  border-radius: 20px;
+  cursor: pointer;
+  margin: 10px;
+  position: absolute;
+`;
 
 const LinkDiv = styled.div`
   text-align: center;
-  padding-top: 33px;
-`
-
-
+  padding-top: 10px;
+`;
 
 
 
 function App() {
-
-
   const videoRef = useRef(null);
-  const projectVideo = `https://www.youtube.com/watch?v=sPyAQQklc1s`;
+  const projectVideo = 'https://www.youtube.com/watch?v=sPyAQQklc1s';
 
   useEffect(() => {
-    // Accede al elemento de video utilizando videoRef.current
     const videoElement = videoRef.current;
-
-    // Verifica si el video está pausado y si no lo está, llama al método play()
     if (videoElement && videoElement.paused) {
       videoElement.play();
     }
-
-   
   }, []);
 
-  const videoElement = videoRef.current;
-
-    // Verifica si el video está pausado y si no lo está, llama al método play()
-    if (videoElement && videoElement.paused) {
-      videoElement.play();
-    }
-
   return (
-   
-  
-  
-             
-<PlayerWrapper>
+    <PlayerWrapper>
+      <ContainerTest>
+        <LinkDiv>
+          <a href='https://realidad360.com.ar/' target='_blank'>
+            CLICK
+          </a>
+        </LinkDiv>
+      </ContainerTest>
 
+      <ReactPlayer
+        url={projectVideo}
+        controls
+        config={{ file: { forceVideo: true } }}
+        width='100%'
+      />
 
-
-
-
-
-<ContainerTest>
-<LinkDiv>
-<a href='https://realidad360.com.ar/' target='_BLANK' >CLICK </a>
-</LinkDiv>
-
-</ContainerTest>
-
-
-<StyledVideoPlayer
-                    className="video"
-                    url={projectVideo}
-                    controls
-                    config={{ file: { forceVideo: true } }}
-                    width="100%"
-                    height="800px"
-                   
-                   
-                  />
-
-
-</PlayerWrapper>
-
-  
      
- 
-   
+    </PlayerWrapper>
   );
 }
 
